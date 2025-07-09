7360111310cc123502fae8364bcbf425cc2d98ce
igaserx <gasseralaa11@gmail.com>
2025-07-10 01:02:34 +0300

    add Newsreader font

---
+++ lib/core/constants/text_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+final newsTextTheme = TextTheme(
+  headlineLarge: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  ),
+  headlineMedium: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  ),
+  titleMedium: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  ),
+  bodyLarge: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  ),
+  bodyMedium: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  ),
+  labelLarge: const TextStyle(
+    fontFamily: 'Newsreader',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  ),
+);
