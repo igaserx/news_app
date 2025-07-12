f4b019c8864f07d1d2ed66b3f7862ef1b083c05d
igaserx <gasseralaa11@gmail.com>
2025-07-13 00:03:06 +0300

    - add interceptor - getNewsByCategory method

---
+++ lib/core/database/api/end_points.dart
@@ -0,0 +1,13 @@
+class EndPoints {
+ static const String baserUrl = "https://newsapi.org/v2/";
+ static const String endPoint = "everything?";
+ static const String apiKey = "b2cf1cd473674cc9bf5d2e93df93a6de";
+
+}
+
+class ApiKeys { 
+ static const String  image = "urlToImage";
+ static const String  title = "title";
+ static const String  subTitle = "description";
+ static const String  url = "url";
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:news/core/database/api/app_interceptor.dart';
+import 'package:news/core/database/api/end_points.dart';
+import 'package:news/models/news_model.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+
+   ApiService(){    
+   dio.interceptors.add(AppInterceptor());
+   }
+
+  Future<List<NewsModel>> getNewsByCategory(String category ) async {
+    final url = "${EndPoints.baserUrl}${EndPoints.endPoint}apiKey=${EndPoints.apiKey}&language=en&q=$category";
+    try {
+      final response = await dio.get(url);
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData["articles"];
+      return articles.map((article) => NewsModel.formJson(article)).toList();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
