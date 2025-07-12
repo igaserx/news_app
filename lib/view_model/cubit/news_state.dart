7ff98a3c90f23f17dee1a822b19be04df3b6c450
igaserx <gasseralaa11@gmail.com>
2025-07-13 00:08:08 +0300

    News States

---
+++ lib/view_model/cubit/news_state.dart
@@ -0,0 +1,17 @@
+part of 'news_cubit.dart';
+
+@immutable
+sealed class NewsState {}
+
+final class NewsInitial extends NewsState {}
+final class NewsLoading extends NewsState{}
+final class NewLoaded extends NewsState{
+  final NewsModel news ;
+
+  NewLoaded({required this.news});
+}
+final class NewsFailure extends NewsState{
+  final String errMessage ;
+
+  NewsFailure({required this.errMessage});
+}
